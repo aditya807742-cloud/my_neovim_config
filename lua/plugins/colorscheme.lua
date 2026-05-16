59c7ed18778d2a7fddbbfc294cb8f21d937b978e
Aditya <aditya807742@gmail.com>
2026-05-17 00:57:06 +0530

    Theme change for lualine

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,7 +3,7 @@ return {
 	"rose-pine/neovim",
 --	name = "rose-pine",
 	config = function()
-		vim.cmd("colorscheme retrobox")
+		vim.cmd("colorscheme habamax")
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
     vim.cmd [[
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,7 +3,7 @@ return {
 	config= function()
 	require('lualine').setup({
         options = {
-    theme = 'onedark',
+    theme = 'powerline_dark',
     icons_enabled = true,
     component_separators = '|',
     section_separators = '',
